965ce0aebf1e004d2ef766174b06ace317002eb3
Tsegay G <tsegay2017@gmail.com>
2019-02-28 10:31:59 -0500

    first commit

---
+++ src/main/java/com/exchange/client/TransactionTO.java
@@ -0,0 +1,34 @@
+package com.exchange.client;
+
+public class TransactionTO {
+	private long accountId;
+	private String currencyFrom;
+	private String currencyTo;
+	private double amountFrom;
+	public long getAccountId() {
+		return accountId;
+	}
+	public void setAccountId(long accountId) {
+		this.accountId = accountId;
+	}
+	public String getCurrencyFrom() {
+		return currencyFrom;
+	}
+	public void setCurrencyFrom(String currencyFrom) {
+		this.currencyFrom = currencyFrom;
+	}
+	public String getCurrencyTo() {
+		return currencyTo;
+	}
+	public void setCurrencyTo(String currencyTo) {
+		this.currencyTo = currencyTo;
+	}
+	public double getAmountFrom() {
+		return amountFrom;
+	}
+	public void setAmountFrom(double amountFrom) {
+		this.amountFrom = amountFrom;
+	}
+	 
+
+}
